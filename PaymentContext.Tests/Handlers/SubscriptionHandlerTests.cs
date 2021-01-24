ea5530532ae88f1b7f096a9f58852a465c414ccd
Rodrigo Yamamoto <rr.yamamoto@outlook.com>
2021-01-24 22:35:27 +0000

    tests: add unit test

---
+++ PaymentContext.Tests/Handlers/SubscriptionHandlerTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.Commands;
+using PaymentContext.Domain.Enums;
+using PaymentContext.Domain.Handlers;
+using PaymentContext.Tests.Mocks;
+
+namespace PaymentContext.Tests.Handlers
+{
+    [TestClass]
+    public class SubscriptionHandlerTests
+    {
+        [TestMethod]
+        public void ShouldReturnErrorWhenDocumentExists()
+        {
+            var handler = new SubscriptionHandler(new FakeStudentRepository(), new FakeEmailService());
+            var command = new CreateBoletoSubscriptionCommand
+            {
+                FirstName = "Bruce",
+                LastName = "Wayne",
+                Document = "99999999999",
+                Email = "email@gmail.com",
+
+                BarCode = "123456789",
+                BoletoNumber = "123456789",
+
+                PaymentNumber = "123121",
+                PaidDate = DateTime.Now,
+                ExpireDate = DateTime.Now.AddMonths(1),
+                Total = 60,
+                TotalPaid = 60,
+                Payer = "WAYNE CORP",
+                PayerDocument = "12345678911",
+                PayerDocumentType = EDocumentType.CPF,
+                PayerEmail = "batman@dc.com",
+
+                Street = "asdasd",
+                Number = "sdaasda",
+                Neighborhood = "dasdas",
+                City = "sdasd",
+                State = "dasdas",
+                Country = "sadasd",
+                ZipCode = "123456789"
+            };
+
+            handler.Handle(command);
+            Assert.AreEqual(false, handler.Valid);
+        }
+    }
+}
---
+++ PaymentContext.Tests/Mocks/FakeStudentRepository.cs
@@ -0,0 +1,28 @@
+﻿using PaymentContext.Domain.Entities;
+using PaymentContext.Domain.Repository;
+
+namespace PaymentContext.Tests.Mocks
+{
+    public class FakeStudentRepository : IStudentRepository
+    {
+        public bool DocumentExists(string document)
+        {
+            if (document == "99999999999")
+                return true;
+
+            return false;
+        }
+
+        public bool EmailExists(string email)
+        {
+            if (email == "email@email.com")
+                return true;
+
+            return false;
+        }
+
+        public void CreateSubscription(Student student)
+        {
+        }
+    }
+}
